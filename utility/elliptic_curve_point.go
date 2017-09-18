3f724ba03492e9dba5d701fb0d621841754e49d6
revett <charlierevett@gmail.com>
2017-09-18 18:50:27 +0100

    Elliptic Curve implementation

---
+++ utility/elliptic_curve_point.go
@@ -0,0 +1,30 @@
+// Original work completed by @vsergeev: https://github.com/vsergeev/btckeygenie
+// Expanded upon here under MIT license.
+
+package utility
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+)
+
+type (
+	// EllipticCurvePoint represents a point on an elliptic curve.
+	EllipticCurvePoint struct {
+		X *big.Int
+		Y *big.Int
+	}
+)
+
+// Format encodes the bytes of an EllipticCurvePoint for debugging.
+func (p EllipticCurvePoint) Format() string {
+	if p.X == nil && p.Y == nil {
+		return "(inf,inf)"
+	}
+
+	encodedX := hex.EncodeToString(p.X.Bytes())
+	encodedY := hex.EncodeToString(p.Y.Bytes())
+
+	return fmt.Sprintf("(%s,%s)", encodedX, encodedY)
+}
