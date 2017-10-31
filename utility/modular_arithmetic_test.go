e7ab1f1cdce1c07af5cb6258616fd925acccccc6
Aleksandr Bezobchuk <abezobchuk@gmail.com>
2017-10-30 20:57:50 -0400

    Update ModularArithmetic unit tests to actually use big integers

--- utility/modular_arithmetic_test.go
+++ utility/modular_arithmetic_test.go
@@ -13,31 +13,31 @@ func TestModularArithmeticAdd(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				Y           *big.Int
-				P           *big.Int
+				x           *big.Int
+				y           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Zero",
-					X:           big.NewInt(0),
-					Y:           big.NewInt(0),
-					P:           big.NewInt(1),
-					out:         big.NewInt(0),
+					x:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					y:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					p:           hexToInt("0000000000000000000000000000000000000000000000000000000000000001"),
+					out:         hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
 				},
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					Y:           big.NewInt(3500),
-					P:           big.NewInt(34),
-					out:         big.NewInt(12),
+					x:           hexToInt("50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"),
+					y:           hexToInt("2cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"),
+					p:           hexToInt("5dbcd5dfea550eb4fd3b5333f533f086bb5267c776e2a1a9d8e84c16a6743d82"),
+					out:         hexToInt("1f9dd51a9486426f2d4b600a738a5ff0b5fe8dfc487b0e4ac78fd0f0743f1176"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Add(testCase.X, testCase.Y, testCase.P)
+				result := ma.Add(testCase.x, testCase.y, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
@@ -50,31 +50,31 @@ func TestModularArithmeticExp(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				Y           *big.Int
-				P           *big.Int
+				x           *big.Int
+				y           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Zero",
-					X:           big.NewInt(0),
-					Y:           big.NewInt(0),
-					P:           big.NewInt(1),
-					out:         big.NewInt(0),
+					x:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					y:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					p:           hexToInt("0000000000000000000000000000000000000000000000000000000000000001"),
+					out:         hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
 				},
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					Y:           big.NewInt(3500),
-					P:           big.NewInt(34),
-					out:         big.NewInt(4),
+					x:           hexToInt("50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"),
+					y:           hexToInt("2cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"),
+					p:           hexToInt("5dbcd5dfea550eb4fd3b5333f533f086bb5267c776e2a1a9d8e84c16a6743d82"),
+					out:         hexToInt("02fafb8697960637249e54975cd64bc49ffb4a47a0fcdcbc375081bda8a75cf14"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Exp(testCase.X, testCase.Y, testCase.P)
+				result := ma.Exp(testCase.x, testCase.y, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
@@ -87,28 +87,28 @@ func TestModularArithmeticInverse(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				P           *big.Int
+				x           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Zero",
-					X:           big.NewInt(0),
-					P:           big.NewInt(0),
-					out:         big.NewInt(0),
+					x:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					p:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					out:         hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
 				},
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					P:           big.NewInt(3500),
-					out:         big.NewInt(3497),
+					x:           hexToInt("50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"),
+					p:           hexToInt("5dbcd5dfea550eb4fd3b5333f533f086bb5267c776e2a1a9d8e84c16a6743d82"),
+					out:         hexToInt("2ddbccee5484a3a2d082b6abe1b6d2c6622bbb15f1497aa7f4cd1371230a4ae"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Inverse(testCase.X, testCase.P)
+				result := ma.Inverse(testCase.x, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
@@ -121,31 +121,31 @@ func TestModularArithmeticMul(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				Y           *big.Int
-				P           *big.Int
+				x           *big.Int
+				y           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Zero",
-					X:           big.NewInt(0),
-					Y:           big.NewInt(0),
-					P:           big.NewInt(1),
-					out:         big.NewInt(0),
+					x:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					y:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					p:           hexToInt("0000000000000000000000000000000000000000000000000000000000000001"),
+					out:         hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
 				},
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					Y:           big.NewInt(3500),
-					P:           big.NewInt(34),
-					out:         big.NewInt(6),
+					x:           hexToInt("50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"),
+					y:           hexToInt("2cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"),
+					p:           hexToInt("5dbcd5dfea550eb4fd3b5333f533f086bb5267c776e2a1a9d8e84c16a6743d82"),
+					out:         hexToInt("b3ca04921b3d21cb028a1cf9d714b3985353e8ce23c195013aa91ed7eb3bde2"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Mul(testCase.X, testCase.Y, testCase.P)
+				result := ma.Mul(testCase.x, testCase.y, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
@@ -158,22 +158,22 @@ func TestModularArithmeticSqrt(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				P           *big.Int
+				x           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					P:           big.NewInt(3503),
-					out:         big.NewInt(2488),
+					x:           hexToInt("3e8"),
+					p:           hexToInt("daf"),
+					out:         hexToInt("9b8"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Sqrt(testCase.X, testCase.P)
+				result := ma.Sqrt(testCase.x, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
@@ -186,31 +186,31 @@ func TestModularArithmeticSub(t *testing.T) {
 		t.Run("HappyCase", func(t *testing.T) {
 			testCases := []struct {
 				description string
-				X           *big.Int
-				Y           *big.Int
-				P           *big.Int
+				x           *big.Int
+				y           *big.Int
+				p           *big.Int
 				out         *big.Int
 			}{
 				{
 					description: "Zero",
-					X:           big.NewInt(0),
-					Y:           big.NewInt(0),
-					P:           big.NewInt(1),
-					out:         big.NewInt(0),
+					x:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					y:           hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
+					p:           hexToInt("0000000000000000000000000000000000000000000000000000000000000001"),
+					out:         hexToInt("0000000000000000000000000000000000000000000000000000000000000000"),
 				},
 				{
 					description: "Nonzero",
-					X:           big.NewInt(1000),
-					Y:           big.NewInt(3500),
-					P:           big.NewInt(34),
-					out:         big.NewInt(16),
+					x:           hexToInt("50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"),
+					y:           hexToInt("2cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"),
+					p:           hexToInt("5dbcd5dfea550eb4fd3b5333f533f086bb5267c776e2a1a9d8e84c16a6743d82"),
+					out:         hexToInt("23b1cab216340bf03549c4f77a923001f92db2054e52f246ba9bec09e202f7ac"),
 				},
 			}
 
 			ma := utility.NewModularArithmetic()
 
 			for _, testCase := range testCases {
-				result := ma.Sub(testCase.X, testCase.Y, testCase.P)
+				result := ma.Sub(testCase.x, testCase.y, testCase.p)
 
 				assert.Equal(t, testCase.out, result)
 			}
